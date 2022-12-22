00c4e9661f4d2d8ee8be449c23f2f4d84aec9a11
Jordan Tannenbaum <jordantannen@gmail.com>
2022-12-22 10:52:57 -0500

    Add boyerMoore() tests

--- BoyerMoore/src/Main.java
+++ BoyerMoore/src/Main.java
@@ -2,12 +2,13 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        CharSequence pattern = "abacab";
-        CharSequence text = "abacbabadcabacab";
+        CharSequence pattern = "cccccc";
+        CharSequence text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
         CharacterComparator comparator = new CharacterComparator();
         System.out.println(pattern);
 
         System.out.println(PatternMatching.boyerMoore(pattern, text, comparator));
+        System.out.println(comparator.getComparisonCount());
 
     }
 }
