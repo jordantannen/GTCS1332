6ed73ca2a682212deba5b15314d19b1e77dcb99f
Jordan Tannenbaum <jordantannen@gmail.com>
2022-12-21 23:27:00 -0500

    Add Main() for testing

---
+++ BoyerMoore/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        CharSequence charSequence = "Happy";
+        System.out.println(charSequence);
+
+        System.out.println(PatternMatching.buildLastTable(charSequence));
+    }
+}
